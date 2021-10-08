ff803aefe56a4c95d0739c79a5c97569c0f0b20d
DrazhinUstin <zmagar91@gmail.com>
2021-10-08 18:16:34 +0300

    Some DOMContentLoaded changes

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ import * as utils from './modules/utils.js';
 import setConverter from './modules/setConverter.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const data = await utils.fetchData(`https://freecurrencyapi.net/api/v2/latest?apikey=${utils.API_KEY}`);
     const settings = utils.getFromStorage('settings');
-    setConverter(data, settings);
     utils.setThemeSwitcher(settings);
+    const data = await utils.fetchData(`https://freecurrencyapi.net/api/v2/latest?apikey=${utils.API_KEY}`);
+    setConverter(data, settings);
 });
 
 window.addEventListener('load', utils.hidePreloader);
